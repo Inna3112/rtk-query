aa5b8cdab5cf08b0e29bee136823510473f33826
Inna3112 <innula3112@gmail.com>
2022-11-10 12:24:56 +0200

    some changes added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import {useGetGoodsQuery, useAddProductMutation, useDeleteProductMutation} from './store';
 import './App.css';
 
 function App() {
+    const [count, setCount] = useState('');
+    const [newProduct, setNewProduct] = useState('');
+    const { data = [], isLoading } = useGetGoodsQuery(count);
+    const [addProduct, {isError}] = useAddProductMutation();
+    const [deleteProduct] = useDeleteProductMutation();
+
+    const handleAddProduct = async () => {
+        if (newProduct) {
+            await addProduct({name: newProduct}).unwrap();
+            setNewProduct('')
+        }
+    }
+
+    const handleDeleteProduct = async (id) => {
+        deleteProduct(id).unwrap();
+    }
+
+    if (isLoading) return <div>Loading...</div>
+    if (!data) return <div>Missing post!</div>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <select value={count} onChange={(e) => setCount(e.currentTarget.value)}>
+            <option value="''">all</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+        </select>
+        <input type="text"
+               value={newProduct}
+               onChange={(e) => setNewProduct(e.currentTarget.value)}/>
+        <button type="submit" onClick={() => handleAddProduct()}>+</button>
+        <ul>
+          {data.map(item => (
+              <li key={item.id}>{item.name}
+                  <span
+                  onClick={() => handleDeleteProduct(item.id)}
+                  style={{cursor: 'pointer'}}
+                  >
+                      -
+                  </span>
+              </li>
+          ))}
+      </ul>
     </div>
   );
 }
---
+++ src/store/goodsAPI.js
@@ -0,0 +1,34 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+export const goodsAPI = createApi({
+    reducerPath: 'goodsAPI',
+    tagTypes: ['Products'],
+    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3001/'}),
+    endpoints: (build => ({
+        //add prefix use and suffix query
+        getGoods: build.query({
+            query: (limit= '') => `goods?${limit && `_limit=${limit}`}`,
+            providesTags: (result, error, arg) =>
+                result
+                    ? [...result.map(({ id }) => ({ type: 'Products', id })), {type: 'Products', id: 'LIST'}]
+                    : [{type: 'Products', id: 'LIST'}],
+        }),
+        addProduct: build.mutation({
+            query: (body) => ({
+                url: 'goods',
+                method: 'POST',
+                body,
+            }),
+            invalidatesTags: [{ type: 'Products', id: 'LIST' }],
+        }),
+        deleteProduct: build.mutation({
+            query: (id) => ({
+                url: `goods/${id}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: [{ type: 'Products', id: 'LIST' }],
+        })
+    }))
+});
+
+export const {useGetGoodsQuery, useAddProductMutation, useDeleteProductMutation} = goodsAPI;
